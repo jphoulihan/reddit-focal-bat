f7f8ee519a0d7d89cf043db9cee3a6b0589e1491
JPFreely <jpshoulihan@gmail.com>
2021-11-24 17:56:17 +0000

    randomness added to pos list, phrase added to bot reply, phrase formatted

--- readme.md
+++ readme.md
@@ -1,24 +1,29 @@
-# FOCALBAT 
+# FOCAL-BAT 
 
 ## Description 
-_focalbat is a comment bot that translates words from English to Irish_
+_focal-bat is a personalized word of the day comment bot_<br/><br/>
+_this comment bot responds to users and translates a word they used in their comment from English to Irish_
 
 ## Project Goal
-_to promote Irish by giving the language more exposure on a platfrom comprised of 448k members_<br/><br/>
+_to promote Irish by giving the language more exposure on a forum for ireland, comprised of 448k members_<br/><br/>
 
 ### **Requirements**
 ---
 * use PRAW to create a reddit instance, get desired subreddit
 * get top post of the day
-* sort comments by top / best / controversial for example
-* store comment (by len ?)
-* Use spacy library to POS (part of speech) tag on the comment content
-* insert first instance of Noun or Verb or Adjective into teanglann foclóir search (get request)
+* get top comment in that post
+* store comment
+* Use natural language processing to get POS (part of speech) on the comment content
+* insert first instance of Noun or Verb or Adjective etc. into teanglann foclóir (dictionary) search (get request)
 * if return contains a dictionary entry continue
-  * else repeat POS tagging to get next Noun or Verb until dictionary contains entry
-* Scrape translation and 1 example from teanglann result
+  * else repeat POS tagging to get next Noun or Verb etc. until the dictionary yields a result
+* Use web scraping to get the dictionaries result content
 * construct the focal bat response with this data
-* if thread top comment contains user focal-bat it has been visited by the bot 
+  * response composed of:
+    * translation
+    * sample phrase
+    * link to dictionary result page
+* if the top post of the day already contains a focal-bat response it has been visited by the bot 
   * do not comment<br/>
 
 ### **Tech Used**
--- reddit-focal-bat.py
+++ reddit-focal-bat.py
@@ -1,20 +1,20 @@
-from re import S
-import requests
-import spacy
+import os
 import praw
+import spacy
+import random
+import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
+ID = os.getenv('CLIENT_ID')
+SECRET = os.getenv('CLIENT_SECRET')
+USERNAME = os.getenv('USERNAME')
 
 nlp = spacy.load('en_core_web_sm')
 
 def main():
 
-    ID = os.getenv('CLIENT_ID')
-    SECRET = os.getenv('CLIENT_SECRET')
-    USERNAME = os.getenv('USERNAME')
 
     reddit = praw.Reddit(
         user_agent= "Comment Extraction (by u/USERNAME)",
@@ -38,25 +38,33 @@ def main():
     #wrapping algorithm in a loop which checks if the bot has already replied, if bot username is found in child comments, break else reply
     for reply in child_comments:
         if reply.author == 'focal_bat':
-            print('focal_bat response FOUND in',len(child_comments),'replies')
+            print('focal_bat response FOUND in',len(child_comments),'replies, program exited')
             break
     else:
-        print('focal_bat response NOT FOUND in',len(child_comments),'replies\n')
+        print('\nfocal_bat response NOT FOUND in',len(child_comments),'replies, continuing program...\n')
         top_comment_author = 'u/'+str(sub_obj[0].comments[0].author)
         top_comment_body = nlp(f'{sub_obj[0].comments[0].body}') # top comment body processed by spacy nlp
-        test_string_body = nlp('there was a teleporting gadget in the ufo in the garden, yesterday')
+        # test_string_body = nlp('there was a teleporting thingamajig in the ufo, I shudda chucked it in the fryer, yesterday')
 
         word_pos = {}
-        pos_list = ['NOUN', 'VERB', 'PROPN', 'ADJ']
         #test string loop
         # for token in test_string_body:
         for token in top_comment_body:
             word_pos[token.lemma_] = f'{token.pos_}' # populating a dictionary with key (lemmatization) value (part of speech) pairs 
         
+        print('Top comment from top post of the day, after part of speech word processing: \n')
         print(word_pos)
+        print('\n')
 
-        #if soup page.content has div class 'nojoy' append the searched word to fail list
+        #if soup page.content has div class 'nojoy' append the searched word to fail list, move to next word_pos translation search
         word_searched_fail = [] 
+        pos_list = ['NOUN', 'VERB', 'PROPN', 'ADJ', 'ADV', 'CCONJ', 'DET']
+
+        print(pos_list)
+        random.shuffle(pos_list)
+        print(pos_list)
+        print('\n')
+
         for word, pos in word_pos.items():
             if pos in pos_list:
                 if word in word_searched_fail:
@@ -72,29 +80,30 @@ def main():
             
         translation_scrape = soup.find('span', attrs={'class': 'eid trg clickable'})
         examples_scrape = soup.find_all('div', attrs={'class': 'ex'}, limit=10)
+        translated_word = str(translation_scrape.text.partition(' ')[0])
 
-        translation = str(translation_scrape.text.partition(' ')[0])
-        # print(type(translation))
-        print(translation.lower())
+        print(f'BÉARLA {word}, GAEILGE {translated_word}\n')
 
-        
+        raw_example_list = [ex.text for ex in examples_scrape] 
+        formatted_example_list = []
+        for example_str in raw_example_list:
+            for symbol in example_str:
+                if ord(symbol) == 187:
+                    new_str = slice(example_str.index(symbol)+1, len(example_str))
+                    formatted_example_list.append(example_str[new_str].strip())
 
-        raw_example_list = [ex.text for ex in examples_scrape]
-        
-        for count, value in enumerate(raw_example_list):
-            print(count, value.strip())
-        
-        for i in raw_example_list:
-            for j in i:
-                if translation.lower() in j:
-                    print(j)
-                    break
+        print('Sentence example list for response :\n')
+        print(formatted_example_list)
+        print('\n')
+
+        random_example_sentence = formatted_example_list[random.randrange(len(formatted_example_list))] 
             
-        response = str(top_comment_author)+', seo duit an focal '+searched_word+' as gaeilge: '+translation_scrape.text.partition(' ')[0]
+        response = str(top_comment_author)+', seo duit an focal '+searched_word+' as gaeilge: '+translated_word
+        example = f'Mar shampla: {random_example_sentence}'
         search_further = f'https://www.teanglann.ie/en/eid/{word}'
     
-        print('focal_bat repsonse: \n')
-        print(response+'\nLike to learn more? Go on, go on, go on...', search_further)
+        print('focal_bat response: \n')
+        print(response+'\n'+example+'\nLike to learn more? Go on, go on, go on...', search_further)
         
     
 if __name__ == "__main__":
