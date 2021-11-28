7ca7aa6ccb16e8f70beeab2dcc190a156aa2be3c
JPFreely <jpshoulihan@gmail.com>
2021-11-28 18:02:23 +0000

    cleaner code with comprehensive lists

--- reddit-focal-bat.py
+++ reddit-focal-bat.py
@@ -15,7 +15,6 @@ nlp = spacy.load('en_core_web_sm')
 
 def main():
 
-
     reddit = praw.Reddit(
         user_agent= "Comment Extraction (by u/USERNAME)",
         client_id= ID,
@@ -24,14 +23,12 @@ def main():
         password= PASSWORD
     )
 
-    sub_obj = [] # stores the top thread of the day submission object
-    subreddit = reddit.subreddit("botwatch").top("all", limit=1) #retrieves the top submission as limit is 1
+    submission_obj = [] # stores the top thread of the day submission object
+    subreddit = reddit.subreddit("ireland").top("day", limit=1) #retrieves the top submission as limit is 1
 
-    for submission in subreddit:
-        print('\nPost of the day Submission Title:',submission.title)
-        sub_obj.append(reddit.submission(id=f'{submission}'))
+    submission_obj = [reddit.submission(id=f'{sub}') for sub in subreddit] 
     
-    parent = reddit.comment(f'{sub_obj[0].comments[0]}')
+    parent = reddit.comment(f'{submission_obj[0].comments[0]}')
     parent.refresh()
     parent.replies.replace_more()
     child_comments = parent.replies.list() # all top comment replies converted to list
@@ -43,56 +40,34 @@ def main():
             break
     else:
         print('\nfocal-bat reply NOT FOUND in',len(child_comments),'replies, continuing program...\n')
-        top_comment_author = 'u/'+str(sub_obj[0].comments[0].author)
-        top_comment_body = nlp(f'{sub_obj[0].comments[0].body}') # top comment body processed by spacy nlp
-        # test_string_body = nlp('there was a teleporting thingamajig in the ufo, I shudda chucked it in the fryer, yesterday')
+        top_comment_author = f'u/ {submission_obj[0].comments[0].author}'
+        top_comment_body = nlp(f'{submission_obj[0].comments[0].body}') # top comment body processed by spacy nlp
 
-        word_pos = {}
-        #test string loop
-        # for token in test_string_body:
-        for token in top_comment_body:
-            word_pos[token.lemma_] = f'{token.pos_}' # populating a dictionary with key (lemmatization) value (part of speech) pairs 
+        word_pos = {token.lemma_ : f'{token.pos_}' for token in top_comment_body } # populating a dictionary with key (lemmatization) value (part of speech) pairs 
         
         print('Top comment from top post of the day, after part of speech word processing: \n')
         print(word_pos)
         print('\n')
 
-        #if soup page_eng_ir.content has div class 'nojoy' append the searched word to fail list, move to next word_pos translation search
-        word_searched_fail = ['ireland', 'Ireland', 'Irish', 'irish'] 
         pos_list = ['NOUN', 'VERB', 'PROPN', 'ADJ', 'ADV', 'CCONJ']
-
-        print(pos_list)
-        random.shuffle(pos_list)
+        random.shuffle(pos_list) #to get different parts of speech translated
         print(pos_list)
         print('\n')
 
-        for word, pos in word_pos.items():
-            if pos in pos_list:
-                if word in word_searched_fail:
-                    continue
-                searched_word = f'''"{word}"''' #qoutationed string which will form part of reply
-                page_eng_to_irish = requests.get(f'https://www.teanglann.ie/en/eid/{word}') # Getting page_eng_ir HTML through request
-                soup = BeautifulSoup(page_eng_to_irish.content, 'html5lib')
-                examples_scrape = soup.find_all('div', attrs={'class': 'ex'}, limit=20) #get sample phrases here, if no examples found add word to fail list
-                if len(word) <= 2 or soup.find('div', attrs={'class': 'nojoy'}) or len(examples_scrape) == 0:
-                    word_searched_fail.append(word)
-                    continue
-                break
+        word, soup, examples_scrape = dict_search(word_pos, pos_list)
+        searched_word = f'''"{word}"'''
         
         print(word,'\n')
+        print(word_pos[word], '\n')
             
             
         translation_scrape = soup.find('span', attrs={'class': 'eid trg clickable'})
         print('Unformatted Translation:')
         print(translation_scrape.prettify())
         print('\n')
-        # translation_scrape = soup.find('span', attrs={'class': 'head'})
-        translated_word_list = str(translation_scrape.text).split()
-        formatted_translated_word = ''
-        for i in translated_word_list:
-            if i == 'm' or i == 'f': #filter out results containing m for masculine of f for feminine  
-                continue
-            formatted_translated_word += str(i)+' '
+
+        translated_word= str(translation_scrape.text).split()
+        formatted_translated_word = ' '.join([('' if char in set("mf") else ' ')+char for char in translated_word]).strip()
 
         print('Formatted translation: ',formatted_translated_word)
         print('\n')
@@ -102,9 +77,8 @@ def main():
         formatted_example_list = []
         for example_str in raw_example_list:
             for symbol in example_str:
-                if ord(symbol) == 187: #phrases begin after right double angle quotes, filter by the ascii value for formatting 
-                    new_str = slice(example_str.index(symbol)+1, len(example_str))
-                    formatted_example_list.append(example_str[new_str].strip())
+                if ord(symbol) == 187: #phrases begin after right double angle quotes, filter by the ascii value for formatting
+                    formatted_example_list.append(example_str[slice(example_str.index(symbol)+1, len(example_str))].strip()) #substring of raw example added to formatted list 
 
         print('Sentence example list for reply :\n')
         print(formatted_example_list)
@@ -120,13 +94,32 @@ def main():
         print(f'{reply}\n{example}\nLike to learn more? Go on, go on, go on...{search_further}')
         lb='\n\n'
 
+        #reply for reddit
         focal_bat_reply = f'{reply}{lb}{example}{lb}Like to learn more? Go on, go on, go on... {search_further}'
+        #parent.reply(focal_bat_reply)
 
-        #actual reply for reddit
-        parent.reply(focal_bat_reply)
 
+#checks if word is in part of speech list and ensures word returns a translation and example phrases from online dictionary
+def dict_search(word_dict, pos_list):
+
+    word_search_fail = ['ireland', 'Ireland', 'Irish', 'irish'] 
+
+    for word, pos in word_dict.items():
+            if pos in pos_list:
+                if word in word_search_fail:
+                    continue
+                page_eng_to_irish = requests.get(f'https://www.teanglann.ie/en/eid/{word}') # Getting page_eng_ir HTML through request
+                soup = BeautifulSoup(page_eng_to_irish.content, 'html5lib')
+                examples_scrape = soup.find_all('div', attrs={'class': 'ex'}, limit=20) #get sample phrases here, if no examples found add word to fail list
+                if len(word) <= 2 or soup.find('div', attrs={'class': 'nojoy'}) or len(examples_scrape) == 0:
+                    word_search_fail.append(word)
+                    continue
+                break
+
+    return word, soup, examples_scrape
         
-    
+
+
 if __name__ == "__main__":
     main()
 
